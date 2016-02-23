2522679afca124262b631cfbc58728921241d486
Klein Geard <kleinmarkgeard@gmail.com>
2016-02-23 21:31:46 +1000

    now supports word 1997 and word 2003 documents

--- src/textextraction/DOCTextExtractor.java
+++ src/textextraction/DOCTextExtractor.java
@@ -2,14 +2,13 @@ package textextraction;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.apache.poi.hwpf.extractor.Word6Extractor;
+import org.apache.poi.hwpf.extractor.WordExtractor;
 
 public class DOCTextExtractor {
 
@@ -25,26 +24,49 @@ public class DOCTextExtractor {
 	
 	public String parseToPlainText(File file) {
 		
-		String text = "";
-		
-	    try {
-	    	
-			InputStream stream = new FileInputStream(file);
-			Word6Extractor extractor = new Word6Extractor(stream);
-			
-			text = extractor.getText();
-			
-			extractor.close();
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	    String text = this.getWord6or95(file);
+	    
+	    if (text == "")
+	    	text = this.getWord1997or2003(file);
 	    
 	    return text;
 	    
 	}
-
+	
+	private String getWord6or95(File file) {
+		try {
+	    	
+			InputStream stream = new FileInputStream(file);
+			Word6Extractor extractor = new Word6Extractor(stream);
+			
+			String text = extractor.getText();
+			
+			extractor.close();
+			
+			return text;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "";
+		} 
+	}	
+	
+	private String getWord1997or2003(File file) {
+		try {
+	    	
+			InputStream stream = new FileInputStream(file);
+			WordExtractor extractor = new WordExtractor(stream);
+			
+			String text = extractor.getText();
+			
+			extractor.close();
+			
+			return text;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "";
+		} 
+	}
 	
 }
