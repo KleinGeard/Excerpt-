570b9401641d2e3de0cb10e83c53d620b404ed38
Klein Geard <kleinmarkgeard@gmail.com>
2016-05-17 13:41:57 +1000

    used try/multi-catch instead of catching each exception individually

--- src/application/Window.java
+++ src/application/Window.java
@@ -55,17 +55,12 @@ public class Window {
 	
 	private void addTheme() {
 		
-			try {
-				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			} catch (InstantiationException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			} catch (UnsupportedLookAndFeelException e) {
-				e.printStackTrace();
-			}
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}			
 		
 	}
 	
